30593e2f6e693ade784bd7f1091b9adc757dbde7
FinalLabs <63569156+FinalLabs@users.noreply.github.com>
2021-08-18 17:52:10 -0400

    Update Hook.h

--- Hook.h
+++ Hook.h
@@ -11,39 +11,14 @@ public:
     {
     public:
         template<typename T1>
-        static std::uintptr_t aslr(const T1 address)
+       static const std::uintptr_t aslr(const T1 address)
         {
             return (address - 0x400000 + reinterpret_cast<std::uintptr_t>(GetModuleHandleA(0)));
         }
     };
 private:
-    static int add_jmp(BYTE* adr, const std::uint32_t func, std::size_t size)
-    {
-        if (size < 5)
-            return 0;
-
-        VirtualProtect(reinterpret_cast<void*>(adr), size, PAGE_EXECUTE_READWRITE, &old);
-        std::uint32_t rel = (static_cast<std::uint32_t>(func) - reinterpret_cast<std::uint32_t>(adr)) - 5;
-
-        *adr = 0xE9;
-
-        *reinterpret_cast<std::uintptr_t*>((reinterpret_cast<std::uintptr_t>(adr + 1))) = rel;
-        VirtualProtect(reinterpret_cast<void*>(adr), size, old, &old);
-
-        return 1;
-    }
-
-    static int remove_jmp(BYTE* adr, const std::uint32_t func, std::size_t size)
-    {
-        if (size < 5)
-            return 0;
-
-        VirtualProtect(reinterpret_cast<void*>(adr), size, PAGE_EXECUTE_READWRITE, &old);
-        std::memcpy(reinterpret_cast<void*>(adr), hook_storage, 5);
-        VirtualProtect(reinterpret_cast<void*>(adr), size, old, &old);
-
-        return 1;
-    }
+    static int add_jmp(BYTE* adr, const std::uint32_t func, std::size_t size);
+    static int remove_jmp(BYTE* adr, const std::uint32_t func, std::size_t size);
 public:
     enum class mode : std::uint32_t
     {
