857e4d7d711a6997d6301337c56778505bb6b38d
FinalLabs <63569156+FinalLabs@users.noreply.github.com>
2021-08-18 17:52:24 -0400

    Update Hook.cpp

--- Hook.cpp
+++ Hook.cpp
@@ -1,5 +1,36 @@
 #include "Hook.h"
 
+BYTE* hook::hook_storage;
+DWORD hook::old;
+
+int hook::add_jmp(BYTE* adr, const std::uint32_t func, std::size_t size)
+{
+    if (size < 5)
+        return 0;
+
+    VirtualProtect(reinterpret_cast<void*>(adr), size, PAGE_EXECUTE_READWRITE, &old);
+    std::uint32_t rel = (static_cast<std::uint32_t>(func) - reinterpret_cast<std::uint32_t>(adr)) - 5;
+
+    *adr = 0xE9;
+
+    *reinterpret_cast<std::uintptr_t*>((reinterpret_cast<std::uintptr_t>(adr + 1))) = rel;
+    VirtualProtect(reinterpret_cast<void*>(adr), size, old, &old);
+
+    return 1;
+}
+
+int hook::remove_jmp(BYTE* adr, const std::uint32_t func, std::size_t size)
+{
+    if (size < 5)
+        return 0;
+
+    VirtualProtect(reinterpret_cast<void*>(adr), size, PAGE_EXECUTE_READWRITE, &old);
+    std::memcpy(reinterpret_cast<void*>(adr), hook_storage, 5);
+    VirtualProtect(reinterpret_cast<void*>(adr), size, old, &old);
+
+    return 1;
+}
+
 void hook::store_bytes(BYTE* adr, int size)
 {
     int sz = size - 1;
@@ -7,6 +38,7 @@ void hook::store_bytes(BYTE* adr, int size)
     for (int i = 0; i <= sz; i++)
     {
         storage[i] = *reinterpret_cast<BYTE*>(adr + i);
+        //printf("%x ", storage[i]);
     }
 
     hook_storage = storage;
@@ -28,6 +60,3 @@ void hook::detour_function(BYTE* adr, const std::uint32_t func, std::size_t size
     }
     }
 }
-
-BYTE* hook::hook_storage;
-DWORD hook::old;
