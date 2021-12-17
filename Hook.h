484be04f97ec2bb0b304cd4ad0ecb490fe3e05eb
final <63569156+FinalLabs@users.noreply.github.com>
2021-12-16 16:01:17 -0800

    Update Hook.h

--- Hook.h
+++ Hook.h
@@ -3,6 +3,11 @@
 
 class hook
 {
+public:
+    struct hstorage
+    {
+        BYTE* bytes;
+    };
 private:
     static BYTE* hook_storage;
     static DWORD old;
@@ -10,8 +15,8 @@ public:
     class utils
     {
     public:
-       template<typename T1>
-       static const std::uintptr_t aslr(const T1 address)
+        template<typename T1>
+        static const std::uintptr_t aslr(const T1 address)
         {
             return (address - 0x400000 + reinterpret_cast<std::uintptr_t>(GetModuleHandleA(0)));
         }
@@ -20,12 +25,6 @@ private:
     static int add_jmp(BYTE* adr, const std::uint32_t func, std::size_t size);
     static int remove_jmp(BYTE* adr, const std::uint32_t func, std::size_t size);
 public:
-    enum class mode : std::uint32_t
-    {
-        create = 1,
-        remove = 0
-    };
-public:
     static void store_bytes(BYTE* adr, int size);
-    static void detour_function(BYTE* adr, const std::uint32_t func, std::size_t size, hook::mode m);
+    static void detour_function(BYTE* adr, const std::uint32_t func, std::size_t size, bool mode);
 };
