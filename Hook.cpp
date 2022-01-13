6f0cf89ebd261604afc1e31ac4618d0cb9655369
final <63569156+FinalLabs@users.noreply.github.com>
2022-01-12 23:04:45 -0800

    Update Hook.cpp

--- Hook.cpp
+++ Hook.cpp
@@ -1,61 +1,43 @@
 #include "Hook.h"
 
-BYTE* hook::hook_storage;
-DWORD hook::old;
+BYTE* Hook::hookStorage;
+DWORD Hook::old;
 
-int hook::add_jmp(BYTE* adr, const std::uint32_t func, std::size_t size)
+void Hook::placeJMP(BYTE* adr, std::uint32_t func, std::size_t size)
 {
-    if (size < 5)
-        return 0;
-
     VirtualProtect(reinterpret_cast<void*>(adr), size, PAGE_EXECUTE_READWRITE, &old);
-    std::uint32_t rel = (static_cast<std::uint32_t>(func) - reinterpret_cast<std::uint32_t>(adr)) - 5;
-
     *adr = 0xE9;
-
-    *reinterpret_cast<std::uintptr_t*>((reinterpret_cast<std::uintptr_t>(adr + 1))) = rel;
+    *reinterpret_cast<std::uintptr_t*>((reinterpret_cast<std::uintptr_t>(adr + 1))) = (static_cast<std::uint32_t>(func) - reinterpret_cast<std::uint32_t>(adr)) - 5;
     VirtualProtect(reinterpret_cast<void*>(adr), size, old, &old);
-
-    return 1;
 }
 
-int hook::remove_jmp(BYTE* adr, const std::uint32_t func, std::size_t size)
+void Hook::removeJMP(BYTE* adr, std::uint32_t func, std::size_t size)
 {
-    if (size < 5)
-        return 0;
-
     VirtualProtect(reinterpret_cast<void*>(adr), size, PAGE_EXECUTE_READWRITE, &old);
-    std::memcpy(reinterpret_cast<void*>(adr), hook_storage, 5);
+    std::memcpy(reinterpret_cast<void*>(adr), hookStorage, 5);
     VirtualProtect(reinterpret_cast<void*>(adr), size, old, &old);
-
-    return 1;
 }
 
-void hook::store_bytes(BYTE* adr, int size)
+void Hook::storeBytes(BYTE* adr, std::size_t size)
 {
-    int sz = size - 1;
-    BYTE* storage = new BYTE[sz];
-    for (int i = 0; i <= sz; i++)
-    {
+    BYTE* storage = new BYTE[size - 1];
+    for (int i = 0; i <= size - 1; i++)
         storage[i] = *reinterpret_cast<BYTE*>(adr + i);
-        //printf("%x ", storage[i]);
-    }
-
-    hook_storage = storage;
+    hookStorage = storage;
 }
 
-void hook::detour_function(BYTE* adr, const std::uint32_t func, std::size_t size, bool mode) 
+void Hook::detourFunction(BYTE* adr, std::uint32_t func, std::size_t size, bool mode)
 {
     switch (mode)
     {
-    case 1:
+    case true:
     {
-        hook::add_jmp(adr, func, size);
+        Hook::placeJMP(adr, func, size);
         break;
     }
-    case 0:
+    case false:
     {
-        hook::remove_jmp(adr, func, size);
+        Hook::removeJMP(adr, func, size);
         break;
     }
     }
